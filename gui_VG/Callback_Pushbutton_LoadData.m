34b02d2920ef9224b82fe40fdd43c64eb1b5bc1a
Zhen JI <zhen_ji@hotmail.com>
2021-01-21 09:28:39 -0600

    TableB all buttons off

--- gui_VG/Callback_Pushbutton_LoadData.m
+++ gui_VG/Callback_Pushbutton_LoadData.m
@@ -18,6 +18,7 @@ fd_VG = fullfile(tempdir, 'VG');
 
     ffn = 'data_MRN-Mrn1234567_Fraction-1_DT-20210113-142422-145655_TE_1953.mat';
     ffn = 'data_MRN-Mrn202101_Fraction-1_DT-20210118-090323-091507_TE_702';
+    ffn = 'data_MRN-Mrn202101_Fraction-2_DT-20210118-091655-092650_TE_595';
     load(ffn)
     tMax = tt(end);
     
--- gui_VG/Callback_Radiobutton_TableB_Box_.m
+++ gui_VG/Callback_Radiobutton_TableB_Box_.m
@@ -29,27 +29,38 @@ end
 yBAll = yA;
 periodBAll = pA;
 
-% GAV
-junk = abs(yA-mean(yA));
-GAV = sum(junk(:))/numel(junk);
-data2.Panel.ParamB.Comp.Text.ParamValue(1).String = num2str(GAV, 2 );
+if isempty(yBAll)
+    data2.Panel.ViewB.Comp.hPlotObj.Avg.Visible = 'off';
+    data2.Panel.ViewB.Comp.hPlotObj.LAVBox.Visible = 'off';
+    data2.Panel.ParamB.Comp.Text.ParamValue(1).String = '';
+    data2.Panel.ParamB.Comp.Text.ParamValue(2).String = '';
+    data2.Panel.ParamB.Comp.Text.ParamValue(3).String = '';
+else
+    % GAV
+    junk = abs(yA-mean(yA));
+    GAV = sum(junk(:))/numel(junk);
+    data2.Panel.ParamB.Comp.Text.ParamValue(1).String = num2str(GAV, 2 );
 
-% PV
-PV = sum(abs(pA-mean(pA)))/numel(pA)*dt;
-data2.Panel.ParamB.Comp.Text.ParamValue(2).String = num2str(PV, 2 );
+    % PV
+    PV = sum(abs(pA-mean(pA)))/numel(pA)*dt;
+    data2.Panel.ParamB.Comp.Text.ParamValue(2).String = num2str(PV, 2 );
 
-% avg
-set(data2.Panel.ViewB.Comp.hPlotObj.Avg, 'YData', mean(yA));
+    % avg
+    set(data2.Panel.ViewB.Comp.hPlotObj.Avg, 'YData', mean(yA));
+    data2.Panel.ViewB.Comp.hPlotObj.Avg.Visible = 'on';
 
-% LAV
-[LAV] = fun_getLAV(yA, LAVBox.y2);
-data2.Panel.ParamB.Comp.Text.ParamValue(3).String = num2str(LAV, 2 );
+    % LAV
+    [LAV] = fun_getLAV(yA, LAVBox.y2);
+    data2.Panel.ParamB.Comp.Text.ParamValue(3).String = num2str(LAV, 2 );
+    data2.Panel.ViewB.Comp.hPlotObj.LAVBox.Visible = 'on';
 
-% viewB axis YLim
-data2.Panel.ViewB.Comp.hAxis.ViewB.YLim = [min(yA(:))*.9 max(yA(:))*1.1];
+    % viewB axis YLim
+    data2.Panel.ViewB.Comp.hAxis.ViewB.YLim = [min(yA(:))*.9 max(yA(:))*1.1];
 
-y1 = min(yA(:))*0.8;
-data2.Panel.ViewB.Comp.hPlotObj.LVBox.Position = [0 y1 avgBnP+2 LAVBox.y2-y1];
+    y1 = min(yA(:))*0.8;
+    data2.Panel.ViewB.Comp.hPlotObj.LVBox.Position = [0 y1 avgBnP+2 LAVBox.y2-y1];
 
-% reorder
-reorderPlotObj(data2.Panel.ViewB.Comp.hAxis.ViewB);
+    % reorder
+    reorderPlotObj(data2.Panel.ViewB.Comp.hAxis.ViewB);
+
+end
