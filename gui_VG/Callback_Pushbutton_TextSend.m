7a2f0c6347b126b506137249afbca0d5bca71a6b
Zhen JI <zhen_ji@hotmail.com>
2021-03-10 18:32:24 -0600

    text send/clear

--- gui_VG/Callback_Pushbutton_TextSend.m
+++ gui_VG/Callback_Pushbutton_TextSend.m
@@ -3,5 +3,10 @@ function Callback_Pushbutton_TextSend(src, evnt)
 global hFig
 data = guidata(hFig);
 
-data.Panel.TextBox.hPanel.Visible = 'on';
+if strcmp(src.Tag, 'Send')
+    data.Panel.TextBox.hPanel.Visible = 'on';
+else
+    data.Panel.TextWin.Comp.Edit.Text.String = '';
+end
+
 data.Panel.TextBox.Comp.Text.Text.String = data.Panel.TextWin.Comp.Edit.Text.String;
